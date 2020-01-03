0ef637b98db6017b30f5d27e7db5631fd6f74127
eNobreg <ezra.nobrega@outlook.com>
2020-01-03 14:35:23 -0500

    [Update] Removed a free statement so it runs

--- main.c
+++ main.c
@@ -7,9 +7,9 @@ int main(int argc, char **argv)
 {
 	char *line = NULL; 
 	char **tokens = NULL;
-	FILE * fp;
-	ssize_t i = 0;
-	size_t buffersize = 0;
+	FILE * fp = NULL;
+	ssize_t i = 0, c = 1;
+	size_t buffersize = 1024; 
 	void (*func)(stack_t **, unsigned int);
 	unsigned int count = 0;
 
@@ -20,22 +20,23 @@ int main(int argc, char **argv)
 	}
 
 	fp = fopen(argv[1], "r");
-	while (i != -1)
+	while (c == 1)
 	{
 		i = getline(&line, &buffersize, fp);
 		if (i != -1)
 		{
-		printf("Got the line\n");
-		printf("Before Parse: %s\n", line);
-		tokens = parse_line(line);
-		printf("After parse: %s\n", tokens[1]);
-		if (tokens[1] != NULL)
-			n = atoi(tokens[1]);
-		func = getop(tokens[0]);
-		printf("Reached\n");
-		func(&list, count);
+			printf("Got the line\n");
+			printf("Before Parse: %s\n", line);
+			tokens = parse_line(line);
+			printf("After parse: %s\n", tokens[1]);
+			if (tokens[1] != NULL)
+				n = atoi(tokens[1]);
+			func = getop(tokens[0]);
+			printf("Reached\n");
+			func(&list, count);
 		}
-		free(line);
+		else
+			c = 0;
 		count++;
 	}
 	fclose(fp);
