cbe19b9f7f218582cf548695cfd824d5f6d7da9e
KFredlund <katihomes@gmail.com>
2020-01-03 21:50:55 +0000

    [Updated] Add sub function

--- utils_add_swap.c
+++ utils_add_swap.c
@@ -22,7 +22,7 @@ void op_add(stack_t **head, unsigned int line_number)
 	if (current)
 	{
 		new = current->next;
-		new->n = i; 
+		new->n = i;
 		(*head)->next->prev = new;
 	}
 	*head = new;
@@ -48,4 +48,32 @@ void op_swap(stack_t **head, unsigned int line_number)
 		hold->next = current;
 	}
 	*head = hold;
+}
+/**
+ * op_sub - subracts the data of the top node from the second,
+ * and creates a new node and replaces them
+ * @head: List node passed in
+ * @line_number: line number var
+ * Return: Never
+ */
+void op_sub(stack_t **head, unsigned int line_number)
+{
+	int i = 0;
+	stack_t *current = *head;
+	stack_t *new;
+
+	new = malloc(sizeof(stack_t));
+	if (!new)
+		new = NULL;
+	i = current->n - current->next->n;
+	new->n = i;
+	new->next = NULL;
+	new->prev = NULL;
+	if (current)
+	{
+		new = current->next;
+		new->n = i;
+		(*head)->next->prev = new;
+	}
+	*head = new;
 }
