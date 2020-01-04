ee68f65689bee669ea77e989a315f613e30fea92
KFredlund <katihomes@gmail.com>
2020-01-04 01:51:04 +0000

    [Update] Create error handling

--- op_filter.c
+++ op_filter.c
@@ -10,22 +10,22 @@ void (*getop(char *s))(stack_t **stack, unsigned int line_number)
 	instruction_t ops[] = {
 		{"push", op_push},
 		{"pall", op_pall},
-		{"pint", op_pint},
+	/*	{"pint", op_pint},*/
 		{"pop", op_pop},
 		{"swap", op_swap},
 		{"add", op_add},
 		{"nop", op_nop},
 		{"sub", op_sub},
-		/*{"div", "op_div"},
-		{"mul", "op_mul"},
-		{"mod", "op_mod"},
-		{"#", "op_comments"},*/
+		{"div", op_div},
+		{"mul", op_mul},
+		{"mod", op_mod},
+	/*	{"#", op_comments},
 		{"pchar", op_pchar},
-		/*{"pstr", "op_pstr"},
-		{"rotl", "op_rotl"},
-		{"rotr", "op_rotr"},
-		{"stack", "op_stack"},
-		{"queue", "op_queue"},*/
+		{"pstr", op_pstr},
+		{"rotl", op_rotl},
+		{"rotr", op_rotr},
+		{"stack", op_stack},
+		{"queue", op_queue},*/
 		{NULL, NULL}
 };
 		int i = 0;
