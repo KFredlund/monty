354974fd830f0d8c5ccbd64236a25e3642cf2df7
eNobreg <ezra.nobrega@outlook.com>
2020-01-04 18:39:03 -0500

    [Change] Made betty compliant

--- linked_utils_2.c
+++ linked_utils_2.c
@@ -1,13 +1,24 @@
 #include "monty.h"
 #include <ctype.h>
 int is_integer(char *str);
+/**
+ * op_pint - Prints head of list
+ * @head: Head of the list
+ * @line_number: line number
+ * Return: Never
+ */
 void op_pint(stack_t **head, unsigned int line_number)
 {
 	if (*head == NULL)
 		handle_error(5, line_number, "hiiiii");
 	printf("%d\n", (*head)->n);
 }
-
+/**
+ * check_push - Check if the input is push
+ * @tokens: The user input tokenized
+ * @line: The line number
+ * Return: Never
+ */
 void check_push(char **tokens, unsigned int line)
 {
 	if (strcmp(tokens[0], "push") == 0)
@@ -18,11 +29,16 @@ void check_push(char **tokens, unsigned int line)
 			handle_error(4, line, "hiiiii");
 	}
 }
-
+/**
+ * is_integer - Checks if the input is an integer
+ * @str: input to check
+ * Return: 1 or 0
+ */
 int is_integer(char *str)
 {
 	unsigned long i = 0;
-	while(i < strlen(str))
+
+	while (i < strlen(str))
 	{
 		if (isdigit(str[i]) == 0 && str[0] != '-')
 			return (1);
