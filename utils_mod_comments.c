cdb1784f39b264beb324a597d2ed71efa4508987
KFredlund <katihomes@gmail.com>
2020-01-04 01:20:08 +0000

    [Updated] Fix mod statement order

--- utils_mod_comments.c
+++ utils_mod_comments.c
@@ -1,17 +1,17 @@
 #include "monty.h"
-/*
-*op_mod - function that computes the rest of the division
-*of the second top element of the stack by the
-*top element of the stack
-*@head: top of stack
-*@line_number: line number passed here
+/**
+* op_mod - function that computes the rest of the division
+* of the second top element of the stack by the
+* top element of the stack
+* @head: top of stack
+* @line_number: line number passed here
 *
 *Return: Never
 */
 void op_mod(stack_t **head, unsigned int line_number)
 {
 	stack_t *current = *head;
-
+	int i = 0;
 	if ((*head)->n == 0)
 	{
 		fprintf(stderr, "L%u: division by zero\n", line_number);
@@ -19,9 +19,11 @@ void op_mod(stack_t **head, unsigned int line_number)
 	}
 	if ((*head)->next->next)
 	{
-		current->next->n = current->next->n % current->n;
-		*head = current->next;
+		i = current->n % current->next->n;
+		current->next->n = i;
+	//	*head = current->next;
 		free(current);
+		*head = (*head)->next;
 	}
 	else
 	{
@@ -29,3 +31,14 @@ void op_mod(stack_t **head, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 }
+/**
+* op_comments - function that handles ignores line prefaced with a '#'
+* @head: top of stack
+* @line_number: line number passed here
+*
+* Return: Never
+*
+void op_comments(stack_t **head, unsigned int line_number)
+{
+	if 
+}*/
