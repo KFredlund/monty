d15c38ed06f231fc056f8ae3037815179fd39075
KFredlund <katihomes@gmail.com>
2020-01-04 01:37:02 +0000

    [Update] Make betty compliant documentation

--- linked_list_utils.c
+++ linked_list_utils.c
@@ -1,36 +1,37 @@
 #include "monty.h"
 
 /**
- * print_dlistint - prints every element within a node of a single linked list
- * @h: List node passed in
- * Return: Count of what was printed.
+ * op_pall - prints all elements of the stack
+ * @head: top of the stack
+ * @line_number: line number
+ * Return: Never
  */
 void op_pall(stack_t **head, unsigned int line_number)
 {
 
 	int i = 0;
 	stack_t *current = list;
-	printf("Entered pall\n");
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
 		i++;
 	}
-	printf("Left Pall\n");
 }
 
 /**
- * push_stack - adds a node
- * @head: Main passed in
- * @n: number to coy
- * Return: Always new
+ * op_push - adds a node
+ * @list_add: Main passed in
+ * @line_number: line number
+ * Return: Never
  */
 void op_push(stack_t **list_add, unsigned int line_number)
 {
 	stack_t *new = malloc(sizeof(stack_t));
 	stack_t *current = *list_add;
-	if (new == NULL)	
+
+	if (new == NULL)
 		exit(EXIT_FAILURE);
 
 	new->n = n;
@@ -39,9 +40,6 @@ void op_push(stack_t **list_add, unsigned int line_number)
 	if (*list_add != NULL)
 		current->prev = new;
 	*list_add = new;
-	printf("Reached in func\n");
-	printf("%d\n", list->n);
-	printf("After func\n");
 }
 /**
  * op_pop - Pop the top element of the stack
@@ -52,6 +50,7 @@ void op_push(stack_t **list_add, unsigned int line_number)
 void op_pop(stack_t **head, unsigned int line_number)
 {
 	stack_t *current;
+
 	if (head == NULL || *head == NULL)
 		exit(EXIT_FAILURE);
 
@@ -63,7 +62,8 @@ void op_pop(stack_t **head, unsigned int line_number)
 }
 /**
  * op_nop - prints every element within a node of a single linked list
- * @h: List node passed in
+ * @head: top of stack
+ * @line_number: line number
  * Return: Count of what was printed.
  */
 void op_nop(stack_t **head, unsigned int line_number)
@@ -77,7 +77,7 @@ void op_nop(stack_t **head, unsigned int line_number)
 	}
 }
 /**
- * free_dlistint - Frees a singly linked list
+ * free_stack - Frees a singly linked list
  * @head: List to free
  * Return: Nothing
  */
