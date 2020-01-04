bfe0e2e8f89f0cc9bca67bc76c1e425194ea239d
eNobreg <ezra.nobrega@outlook.com>
2020-01-04 17:53:13 -0500

    [Updated] New function and checking push

--- linked_utils_2.c
+++ linked_utils_2.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 #include <ctype.h>
-
+int is_integer(char *str);
 void op_pint(stack_t **head, unsigned int line_number)
 {
 	if (*head == NULL)
@@ -12,9 +12,20 @@ void check_push(char **tokens, unsigned int line)
 {
 	if (strcmp(tokens[1], "push") == 0)
 	{
-		if (isdigit(tokens[1]) > 0)
+		if (is_integer(tokens[1]) == 0)
 			n = atoi(tokens[1]);
 		else
 			handle_error(4, line, "hiiiii");
 	}
+}
+
+int is_integer(char *str)
+{
+	int i = 0;
+	
+	while(str[i])
+		if (isdigit(str[i]) == 0 && isdigit(str[i]) != '-')
+			return (1);
+		i++;
+	return (0);
 }
